1e8de42970d91e5e7e0dd03578453ddb00f941e9
kolinhar <kolinhar@users.noreply.github.com>
2022-10-06 14:09:39 +0200

    3 files modified

--- demo.tsx
+++ demo.tsx
@@ -6,9 +6,13 @@ import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import variablesModuleScss from './variables.module.scss';
-import './variables.scss';
-import './variables.module.css';
-import './variables.css';
+import variablesScss from './variables.scss';
+import variablesModuleCss from './variables.module.css';
+import variablesCss from './variables.css';
+const variablesModuleScssR = require('./variables.module.scss');
+const variablesScssR = require('./variables.scss');
+const variablesModuleCssR = require('./variables.module.css');
+const variablesCssR = require('./variables.css');
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 const JsonFormat = (key: string, val: any) => {
@@ -32,15 +36,23 @@ const findMyVar = (varName: string) => {
   return getComputedStyle(document.documentElement).getPropertyValue(varName);
 };
 
+const isGoodValue = (val) => {
+  return val !== '' && JSON.stringify(val) !== '{}' ? (
+    <span className="green">OK</span>
+  ) : (
+    <span className="red">KO</span>
+  );
+};
+
 function MyApp() {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
 
   const [forceWhite, setForceWhite] = useState<'dark' | ''>('');
 
-  console.log("findMyVar('--my-color')", findMyVar('--my-color'));
-  console.log("findMyVar('--my-module-color')", findMyVar('--my-module-color'));
-  console.log('findMyVar("myScssVar")', findMyVar('yhyghn !b myScssVar'));
+  // console.log("findMyVar('--my-color')", findMyVar('--my-color'));
+  // console.log("findMyVar('--my-module-color')", findMyVar('--my-module-color'));
+  // console.log('findMyVar("myScssVar")', findMyVar('myScssVar'));
 
   const changeToWhite = React.useCallback(() => {
     //just add/remove a class to parent element
@@ -135,6 +147,64 @@ function MyApp() {
           secondary dark
         </div>
       </section>
+      <hr />
+      <article>
+        <table>
+          <caption>
+            <h1>Importation depuis une feuille styles</h1>
+          </caption>
+          <thead>
+            <tr>
+              <th></th>
+              <th>variables.css</th>
+              <th>variables.scss</th>
+              <th>variables.module.css</th>
+              <th>variables.module.scss</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th rowSpan={2}>import</th>
+              <th>{isGoodValue(variablesCss)}</th>
+              <th>{isGoodValue(variablesScss)}</th>
+              <th>{isGoodValue(variablesModuleCss)}</th>
+              <th>{isGoodValue(variablesModuleScss)}</th>
+            </tr>
+            <tr>
+              <td>{JSON.stringify(variablesCss)}</td>
+              <td>{JSON.stringify(variablesScss)}</td>
+              <td>{JSON.stringify(variablesModuleCss)}</td>
+              <td>{variablesModuleScss.primaryLightDark}</td>
+            </tr>
+            <tr>
+              <th rowSpan={2}>require</th>
+              <th>{isGoodValue(variablesCssR)}</th>
+              <th>{isGoodValue(variablesScssR)}</th>
+              <th>{isGoodValue(variablesModuleCssR)}</th>
+              <th>{isGoodValue(variablesModuleScssR)}</th>
+            </tr>
+            <tr>
+              <td>{JSON.stringify(variablesCssR)}</td>
+              <td>{JSON.stringify(variablesScssR)}</td>
+              <td>{JSON.stringify(variablesModuleCssR)}</td>
+              <td>{variablesModuleScssR.primaryLightDark}</td>
+            </tr>
+            <tr>
+              <th rowSpan={2}>DOM</th>
+              <th>{isGoodValue(findMyVar('--my-color'))}</th>
+              <th>{isGoodValue(findMyVar('myScssVar'))}</th>
+              <th>{isGoodValue(findMyVar('--my-module-color'))}</th>
+              <th>{isGoodValue(findMyVar('primaryLightDark'))}</th>
+            </tr>
+            <tr>
+              <td>{findMyVar('--my-color')}</td>
+              <td>{findMyVar('myScssVar')}</td>
+              <td>{findMyVar('--my-module-color')}</td>
+              <td>{findMyVar('primaryLightDark')}</td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
     </main>
   );
 }
